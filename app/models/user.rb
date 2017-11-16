def3878d1447049b717953bfcc968c3dda8614e1
acartesc <acartescc@gmail.com>
2017-11-15 21:08:01 -0300

    user registration with username unique and present and name present working

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :name, presence: true
+  validates :username, presence: true, uniqueness: true
+
+  has_many :histories
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
+
   resources :histories
+
+  devise_for :users, controllers: {
+    registrations: "users/registrations"
+  }
 
   root "histories#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
